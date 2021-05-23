ae51c4a555f041f05797c2194103e18a993b5b29
Guill Lo <guill.bout@gmail.com>
2021-05-24 02:49:57 +0800

    move window back in app delegate

--- gha-uitests-synthetize/AppDelegate.swift
+++ gha-uitests-synthetize/AppDelegate.swift
@@ -3,12 +3,28 @@ import SwiftUI
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow?
+    var window: NSWindow!
 
     lazy var appComponent = AppComponent()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         appComponent.setUp()
+
+        NSApplication.shared.hide(self)
+
+        let contentView = ContentView()
+
+
+
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered,
+            defer: false
+        )
+        window.center()
+        window.contentView = NSHostingView(rootView: contentView)
+        window.makeKeyAndOrderFront(nil)
     }
 
 }
