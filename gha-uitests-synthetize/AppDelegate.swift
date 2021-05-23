3753e6bfa6ca8ee1b7a25fa16811dc9de4e145ea
Guill Lo <guill.bout@gmail.com>
2021-05-24 02:39:48 +0800

    add app component

--- gha-uitests-synthetize/AppDelegate.swift
+++ gha-uitests-synthetize/AppDelegate.swift
@@ -1,60 +1,12 @@
-//
-//  AppDelegate.swift
-//  gha-uitests-synthetize
-//
-//  Created by Guillaume Leclerc on 23/05/2021.
-//
-
-import Cocoa
 import SwiftUI
-
-#if !DEBUG
-var eventTapCallback: CGEventTapCallBack = { proxy, _, event, _ in
-    return Unmanaged.passUnretained(event)
-}
-#endif
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
-
+    lazy var appComponent = AppComponent()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered,
-            defer: false
-        )
-        window.center()
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
-        
-        #if !DEBUG
-        guard let eventTap = CGEvent.tapCreate(
-            tap: .cghidEventTap,
-            place: .headInsertEventTap,
-            options: .defaultTap,
-            eventsOfInterest: CGEventMask(1 << CGEventType.keyDown.rawValue),
-            callback: eventTapCallback,
-            userInfo: nil
-        ) else {
-            fatalError("can't create tap")
-        }
-
-        let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
-        CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
-        #endif
+        appComponent.setUp()
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
 
 }
-
