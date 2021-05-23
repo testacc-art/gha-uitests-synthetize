ded4375b09acbe269edd7ed0762783d0bc9225f0
Guill Lo <guill.bout@gmail.com>
2021-05-24 00:48:21 +0800

    enforce terminating the app between tests

--- kindaVimUITests/AccessibilityTextElementAdaptor/ATEA_BaseTests.swift
+++ kindaVimUITests/AccessibilityTextElementAdaptor/ATEA_BaseTests.swift
@@ -5,12 +5,18 @@ class ATEA_BaseTests: XCTestCase {
     var app: XCUIApplication!
 
     override func setUpWithError() throws {
+        XCUIApplication().terminate()
+
         try super.setUpWithError()
 
         continueAfterFailure = false
 
         app = XCUIApplication()
         app.launch()
+    }
+
+    override func tearDown() {
+        XCUIApplication().terminate()
     }
 
 }
