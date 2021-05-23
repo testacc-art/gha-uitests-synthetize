53136d4459935c0be651e6f0b4affc1539b05e3c
Guill Lo <guill.bout@gmail.com>
2021-05-24 00:46:44 +0800

    add super setup in base test class

--- kindaVimUITests/AccessibilityTextElementAdaptor/ATEA_BaseTests.swift
+++ kindaVimUITests/AccessibilityTextElementAdaptor/ATEA_BaseTests.swift
@@ -5,6 +5,8 @@ class ATEA_BaseTests: XCTestCase {
     var app: XCUIApplication!
 
     override func setUpWithError() throws {
+        try super.setUpWithError()
+
         continueAfterFailure = false
 
         app = XCUIApplication()
