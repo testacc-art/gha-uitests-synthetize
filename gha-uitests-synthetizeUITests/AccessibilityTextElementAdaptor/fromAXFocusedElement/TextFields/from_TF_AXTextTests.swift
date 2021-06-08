40c9af3ece8fe79495129db8e622382c1a1bfa13
testacc-art <74711697+testacc-art@users.noreply.github.com>
2021-06-08 17:54:15 +0300

    Update from_TF_AXTextTests.swift

--- gha-uitests-synthetizeUITests/AccessibilityTextElementAdaptor/fromAXFocusedElement/TextFields/from_TF_AXTextTests.swift
+++ gha-uitests-synthetizeUITests/AccessibilityTextElementAdaptor/fromAXFocusedElement/TextFields/from_TF_AXTextTests.swift
@@ -6,6 +6,7 @@ class from_TF_AXTextTests: ATEA_BaseTests {
     func test_that_the_AXText_matches_the_AXUIElement_text_for_TextField() {
         let textInAXFocusedElement = "that's the text that is in the TextField..."
         app.textFields.firstMatch.tap()
+        sleep(2)
         app.textFields.firstMatch.typeText(textInAXFocusedElement)
         app.textFields.firstMatch.typeKey(.leftArrow, modifierFlags: [])
 
