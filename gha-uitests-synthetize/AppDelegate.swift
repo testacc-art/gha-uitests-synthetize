db57126b8622e8df78218db0c8cc9bb2e001a57b
testacc-art <74711697+testacc-art@users.noreply.github.com>
2021-06-10 14:56:40 +0300

    Update AppDelegate.swift

--- gha-uitests-synthetize/AppDelegate.swift
+++ gha-uitests-synthetize/AppDelegate.swift
@@ -6,6 +6,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     lazy var appComponent = AppComponent()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        #if targetEnvironment(simulator)
+        // Disable hardware keyboards.
+        let setHardwareLayout = NSSelectorFromString("setHardwareLayout:")
+        UITextInputMode.activeInputModes
+            .filter({ $0.responds(to: setHardwareLayout) })
+            .forEach { $0.perform(setHardwareLayout, with: nil) }
+        #endif
         appComponent.setUp()
     }
 
