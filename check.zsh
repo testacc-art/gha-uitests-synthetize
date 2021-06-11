f92d2a9e165317855bfbc77797a0b4765c8fe9a7
testacc-art <74711697+testacc-art@users.noreply.github.com>
2021-06-11 17:53:37 +0300

    Create check.zsh

---
+++ check.zsh
@@ -0,0 +1,10 @@
+tell application "QuickTime Player"
+  --activate
+  new screen recording
+  start document 1
+  delay 5
+  xcodebuild clean -scheme "AccessibilityElementAdaptor UI Tests" -configuration "UI Testing" test
+  stop document 1
+  save document 1 in file "/tmp/test.mov"
+  quit
+end tell
