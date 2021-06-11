5ab4c3d747e63792f5adc5e9880285a72302ccb6
testacc-art <74711697+testacc-art@users.noreply.github.com>
2021-06-11 18:09:21 +0300

    Update check.zsh

--- check.zsh
+++ check.zsh
@@ -1,9 +1,23 @@
-tell application "QuickTime Player"
-  --activate
-  new screen recording
-  start document 1
-  delay 5
-  stop document 1
-  save document 1 in file "/tmp/test.mov"
-  quit
+--  # Setup to do a screen recording.
+
+tell application "QuickTime Player" to new screen recording
+
+--  # Start the screen recording.
+
+tell application "System Events" to tell process "Screen Shot"
+    repeat until exists button "Record" of its front window
+        delay 0.1
+    end repeat
+    click button "Record" of its front window
 end tell
+
+--  # Set the time in seconds you want the recording to be.
+
+delay 2
+
+--  # Stop the recording.
+
+tell application "System Events" to ¬
+    click menu bar item 1 ¬
+        of menu bar 1 ¬
+        of application process "screencaptureui"
