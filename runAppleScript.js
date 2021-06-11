ddadc468ed3bc5307fac849b83cd679dd2733e16
testacc-art <74711697+testacc-art@users.noreply.github.com>
2021-06-11 17:54:11 +0300

    Update runAppleScript.js

--- runAppleScript.js
+++ runAppleScript.js
@@ -1,12 +1,20 @@
 import applescript from 'applescript';
- 
+ import child_process from 'child_process'
+import path from 'path';
 // Very basic AppleScript command. Returns the song name of each
 // currently selected track in iTunes as an 'Array' of 'String's.
 import util from 'util'
 const exec = util.promisify(applescript.execFile);
- 
-exec('./check.zsh').then(result => {
- console.log(`result is result`);
+const getPWD = util.promisify(child_process.exec);
+getPWD('pwd').then(result => {
+    console.log(result.stdout);
+    const check = path.join(result.stdout.replace('\n', ''), 'check.zsh')
+    exec(check).then(result => {
+        console.log(`result is result`);
+       }).catch(err => {
+       console.log(err);
+       });
 }).catch(err => {
-console.log(err);
-});
+    console.log(err);
+})
+
