49981e1e4838268261552ab1f72e93ab1521e56e
testacc-art <74711697+testacc-art@users.noreply.github.com>
2021-06-11 17:43:17 +0300

    Update runAppleScript.js

--- runAppleScript.js
+++ runAppleScript.js
@@ -3,21 +3,9 @@ import applescript from 'applescript';
 // Very basic AppleScript command. Returns the song name of each
 // currently selected track in iTunes as an 'Array' of 'String's.
 import util from 'util'
-const exec = util.promisify(applescript.execString);
-let script = `
-tell application "QuickTime Player"
-  --activate
-  new screen recording
-  start document 1
-  delay 5
-  xcodebuild clean -scheme "AccessibilityElementAdaptor UI Tests" -configuration "UI Testing" test
-  stop document 1
-  save document 1 in file "/tmp/test.mov"
-  quit
-end tell
-`;
+const exec = util.promisify(applescript.execFile);
  
-exec(script).then(result => {
+exec('./check.zsh').then(result => {
  console.log(`result is result`);
 }).catch(err => {
 console.log(err);
